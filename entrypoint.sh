f7a3945b6e60d0b688e09c0934dc01cdb69eada0
wilson0611 <wilson.cezar@cbsua.edu.ph>
2025-04-18 09:25:25 +0800

    Updated the entrypoint commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# Setup .env if missing
+if [ ! -f .env ]; then
+    echo ".env not found, copying from .env.dev"
+    cp .env.dev .env
+fi
+
+#Install dependencies if vendor doesn't exist
+if [ ! -f vendor ]; then
+    echo "Installing composer dependencies..."
+    sudo -u www-data composer install --no-dev --optimize-autoloader --no-progress --no-interaction
+fi
+
 # Wait for MySQL to be ready
 echo "Waiting for MySQL to be ready..."
 timeout 30s sh -c 'until nc -z mysql 3306; do sleep 1; done'
